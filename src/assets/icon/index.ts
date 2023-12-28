d647eb82ed72b39a6ff72f19cbce9168650f860b
Beginner-Project <boripats63@nu.ac.th>
2023-12-28 17:50:43 +0700

    feat(icon): create tsx icon

---
+++ src/assets/icon/index.ts
@@ -0,0 +1,7 @@
+export { default as ReactIcon } from "./react";
+export { default as FlutterIcon } from "./flutter";
+export { default as JavaScriptIcon } from "./javascript";
+export { default as KotlinIcon } from "./kotlin";
+export { default as NextJSIcon } from "./nextjs";
+export { default as PythonIcon } from "./python";
+export { default as TypeScriptIcon } from "./typescript";
