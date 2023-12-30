39a6021e6f409d9f5332585afc1b565b14c3584f
Beginner-Project <boripats63@nu.ac.th>
2023-12-30 18:27:57 +0700

    feat(components): clean  and update  class name

--- src/components/projectitem_card/projectitem_card.tsx
+++ src/components/projectitem_card/projectitem_card.tsx
@@ -1,22 +1,40 @@
-import { FC } from "react"
+import { FC } from "react";
 
 interface props {
-    title: string;
-    imageUrl:string;
-    detail: string;
-    shortcuts:string;
-};
-const ProjectItemCard: FC = () => {
-    return <div className="flex flex-col overflow-hidden rounded-md bg-base-100 shadow-xl">
-        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-        <div className="flex flex-col bg-violet-50 px-[18px] py-4">
-            <h2 className="font-semibold text-neutral-900 text-xl">Title Project</h2>
-            <p className="text-neutral-800">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat consequatur iste, excepturi ipsum, laborum accusantium officiis explicabo ducimus error quia ratione, id at molestiae porro! Voluptate possimus magni, quia in modi fugit corporis corrupti minima itaque ipsam similique voluptatum laudantium assumenda harum iusto debitis adipisci. Eum rem animi saepe magnam.</p>
-            <div className="card-actions justify-end">
-                <button className="bg-indigo-700 rounded-md justify-center items-center btn border-none hover:bg-indigo-700 text-white capitalize">link</button>
-            </div>
-        </div>
-    </div>
+  title: string;
+  imageUrl: string;
+  detail: string;
+  shortcuts: string;
 }
+const ProjectItemCard: FC = () => {
+  return (
+    <div className="flex flex-col overflow-hidden rounded-md bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="flex flex-col bg-violet-50 px-[18px] py-4">
+        <h2 className="text-xl font-semibold text-neutral-900">
+          Title Project
+        </h2>
+        <p className="h-32 truncate text-wrap text-neutral-800">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          consequatur iste, excepturi ipsum, laborum accusantium officiis
+          explicabo ducimus error quia ratione, id at molestiae porro! Voluptate
+          possimus magni, quia in modi fugit corporis corrupti minima itaque
+          ipsam similique voluptatum laudantium assumenda harum iusto debitis
+          adipisci. Eum rem animi saepe magnam.
+        </p>
+        <div className="card-actions justify-end">
+          <button className="btn items-center justify-center rounded-md border-none bg-gradient-to-r bg-left capitalize text-white duration-200 ease-out from-pink-700 to-indigo-700">
+            link
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProjectItemCard;
