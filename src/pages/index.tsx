7b824347b50272f35a145ea435c1b5a8b0cd8c7f
Beginner-Project <boripats63@nu.ac.th>
2024-01-01 18:49:10 +0700

    chore(pages): un reuse import in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Landing from './landing'
+import { Inter } from "next/font/google";
+import Landing from "./landing";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
-    >
-      <Landing />
-    </main>
-  )
+    className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
+  >
+    <Landing />
+  </main>
+
+  );
 }
