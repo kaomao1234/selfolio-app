e0452ee66b837c7c525aabda10c52568d9872a61
Beginner-Project <boripats63@nu.ac.th>
2024-01-01 18:48:29 +0700

    feat(pages): create contract and project

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -1,3 +1,11 @@
+import { Footer, Nav } from "@/components";
+
 export default function Project() {
-    return <></>
+  return (
+    <div className="flex min-h-screen w-full flex-col bg-neutral-50">
+      <Nav />
+      <div className="flex flex-col pt-[67px] md:ml-[60px] md:mr-[40px] md:pt-[77px]"></div>
+      <Footer />
+    </div>
+  );
 }
