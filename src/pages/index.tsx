5f6e27aa7c426118ed03eeb98fc2d76f0d484fa4
Beginner-Project <boripats63@nu.ac.th>
2024-01-02 16:56:16 +0700

    feat(pages): remove style in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
 import { Inter } from "next/font/google";
 import Landing from "./landing";
+import { useAppDispatch } from "@/hook";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-    className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
-  >
-    <Landing />
-  </main>
-
+    >
+      <Landing />
+    </main>
   );
 }
