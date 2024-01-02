1bd0c45b0e28211eb6f6a4aef823867b31510543
Beginner-Project <boripats63@nu.ac.th>
2024-01-02 16:49:43 +0700

    feat(components): update size of footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,7 +4,7 @@ import { SiGmail } from "react-icons/si";
 
 const Footer: FC = () => {
   return (
-    <footer className="flex flex-col items-center justify-center bg-gradient-to-r from-pink-700 to-indigo-700 px-10 py-5">
+    <footer className="flex flex-col items-center justify-center bg-gradient-to-r from-pink-700 to-indigo-700 px-10 py-5 w-full mt-10">
       <div className="flex w-full flex-row justify-center gap-10">
         <button>
           <SiGmail className="size-10 text-red-400" />
