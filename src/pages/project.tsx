5244e9a2259878ca0c7c64bd38c6e0420bf7a4a0
Beginner-Project <boripats63@nu.ac.th>
2024-01-02 18:43:29 +0700

    feat(pages): mock data in project page

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -1,9 +1,13 @@
-import { Footer, Nav } from "@/components";
+import { Footer, Nav, ProjectItemCard } from "@/components";
 import { useAppSelector } from "@/hook";
 
 export default function Project() {
-  const selector = useAppSelector((state) => state.navBarManagement);
+  const list = Array.from({ length: 20 }, (v, k) => ProjectItemCard({}));
   return (
-    <div className="flex flex-col pt-[67px] md:ml-[60px] md:mr-[40px] md:pt-[77px]"></div>
+    <div className="flex flex-col pt-[67px] md:ml-[60px] md:mr-[40px] md:pt-[77px]">
+      <div className="grid flex-wrap justify-center gap-x-8 gap-y-8 px-[50px] sm:grid-cols-2 md:grid-cols-3 mt-10">
+        {list}
+      </div>
+    </div>
   );
 }
