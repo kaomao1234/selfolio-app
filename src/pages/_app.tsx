303c0d5983d93670d34aaba81cc77bb4f4ad2828
Beginner-Project <boripats63@nu.ac.th>
2024-01-05 17:52:15 +0700

    chore(pages): remove un use import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { Footer, Nav } from "@/components";
 import { store } from "@/store/store";
 import "@/styles/globals.css";
-import "@/styles/landing.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 
