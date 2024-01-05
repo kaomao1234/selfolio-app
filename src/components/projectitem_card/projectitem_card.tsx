b0bcd8eff4fd61654247442d80da672a70e0f319
Beginner-Project <boripats63@nu.ac.th>
2024-01-05 17:48:22 +0700

    feat(components): update ui in project item card

--- src/components/projectitem_card/projectitem_card.tsx
+++ src/components/projectitem_card/projectitem_card.tsx
@@ -1,16 +1,15 @@
 import { FC } from "react";
 
-
 const ProjectItemCard: FC = () => {
   return (
-     <button className="flex flex-col overflow-hidden rounded-md shadow-xl bg-slate-100 hover:bg-gray-300 ease-out duration-200 hover:-translate-y-2.5">
+    <button className="flex flex-col overflow-hidden rounded-md bg-slate-100 shadow-xl duration-200 ease-out hover:translate-y-[-9px] hover:bg-gray-300 z-1">
       <figure>
         <img
           src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
           alt="Shoes"
         />
       </figure>
-      <div className="flex flex-col px-[18px] py-4 items-start text-start">
+      <div className="flex flex-col items-start px-[18px] py-4 text-start">
         <h2 className="text-xl font-semibold text-neutral-900">
           Title Project
         </h2>
