70db985af76fb5dfa056e361b366ad90f104a03d
Beginner-Project <boripats63@nu.ac.th>
2024-01-05 17:50:48 +0700

    feat(pages): update responsive in project

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -2,10 +2,15 @@ import { Footer, Nav, ProjectItemCard } from "@/components";
 import { useAppSelector } from "@/hook";
 
 export default function Project() {
-  const list = Array.from({ length: 20 }, (v, k) => ProjectItemCard({}));
+  const list = Array.from({ length: 20 }, (v, k) => (
+    <ProjectItemCard key={k} />
+  ));
   return (
-    <div className="flex flex-col pt-[67px] md:ml-[60px] md:mr-[40px] md:pt-[77px]">
-      <div className="grid flex-wrap justify-center gap-x-8 gap-y-8 px-[50px] sm:grid-cols-2 md:grid-cols-3 mt-10">
+    <div className="mx-10 flex flex-col items-start pt-[67px] md:mx-[187px] md:pt-[77px]">
+      <div className="text-2xl font-semibold leading-relaxed tracking-wide text-neutral-900">
+        Projects
+      </div>
+      <div className="mt-10 grid flex-wrap justify-center gap-x-8 gap-y-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
         {list}
       </div>
     </div>
