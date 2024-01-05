4e49e3bd86ddccd76917c9cda9f46354ab230729
Beginner-Project <boripats63@nu.ac.th>
2024-01-05 17:48:49 +0700

    chore(pages): format class name in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import { Footer, Nav } from "@/components";
 import { store } from "@/store/store";
 import "@/styles/globals.css";
+import "@/styles/landing.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <div className="flex  min-h-screen  w-full flex-col items-center bg-neutral-50">
+      <div className="flex min-h-screen w-full flex-col items-center bg-neutral-50">
         <Nav></Nav>
         <Component {...pageProps} />
         <Footer></Footer>
